2afcda0fdd5a990c9762285cb84de6a43c53a8f7
JHarrell1211 <julian1211@gmail.com>
2018-04-04 10:27:39 -0400

    controllers added

---
+++ ParkSomewhereApp/Controllers/PhotosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ParkSomewhereApp.Models;
+
+namespace ParkSomewhereApp.Controllers
+{
+    public class PhotosController : Controller
+    {
+        private ParkSomewhereAppEntities db = new ParkSomewhereAppEntities();
+
+        // GET: Photos
+        public ActionResult Index()
+        {
+            var photos = db.Photos.Include(p => p.Park).Include(p => p.AspNetUser);
+            return View(photos.ToList());
+        }
+
+        // GET: Photos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(photo);
+        }
+
+        // GET: Photos/Create
+        public ActionResult Create()
+        {
+            ViewBag.ParkID = new SelectList(db.Parks, "ParkID", "ParkName");
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Photos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PhotoID,ParkID,Image,UserID")] Photo photo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Photos.Add(photo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ParkID = new SelectList(db.Parks, "ParkID", "ParkName", photo.ParkID);
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", photo.UserID);
+            return View(photo);
+        }
+
+        // GET: Photos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ParkID = new SelectList(db.Parks, "ParkID", "ParkName", photo.ParkID);
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", photo.UserID);
+            return View(photo);
+        }
+
+        // POST: Photos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PhotoID,ParkID,Image,UserID")] Photo photo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(photo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ParkID = new SelectList(db.Parks, "ParkID", "ParkName", photo.ParkID);
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", photo.UserID);
+            return View(photo);
+        }
+
+        // GET: Photos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(photo);
+        }
+
+        // POST: Photos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Photo photo = db.Photos.Find(id);
+            db.Photos.Remove(photo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ParkSomewhereApp/Controllers/ReviewsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ParkSomewhereApp.Models;
+
+namespace ParkSomewhereApp.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private ParkSomewhereAppEntities db = new ParkSomewhereAppEntities();
+
+        // GET: Reviews
+        public ActionResult Index()
+        {
+            var reviews = db.Reviews.Include(r => r.Park).Include(r => r.AspNetUser);
+            return View(reviews.ToList());
+        }
+
+        // GET: Reviews/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        // GET: Reviews/Create
+        public ActionResult Create()
+        {
+            ViewBag.ParkID = new SelectList(db.Parks, "ParkID", "ParkName");
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Reviews/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ReviewID,ParkID,ReviewTitle,ReviewComments,ReviewTimeStamp,Rating,TotalVotes,UserID")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reviews.Add(review);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ParkID = new SelectList(db.Parks, "ParkID", "ParkName", review.ParkID);
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", review.UserID);
+            return View(review);
+        }
+
+        // GET: Reviews/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ParkID = new SelectList(db.Parks, "ParkID", "ParkName", review.ParkID);
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", review.UserID);
+            return View(review);
+        }
+
+        // POST: Reviews/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ReviewID,ParkID,ReviewTitle,ReviewComments,ReviewTimeStamp,Rating,TotalVotes,UserID")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(review).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ParkID = new SelectList(db.Parks, "ParkID", "ParkName", review.ParkID);
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", review.UserID);
+            return View(review);
+        }
+
+        // GET: Reviews/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Review review = db.Reviews.Find(id);
+            db.Reviews.Remove(review);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
